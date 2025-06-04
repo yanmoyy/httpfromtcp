f014e3411e9628a5627d0ff0e6f2731fa2bd377e
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-06-05 05:17:28 +0900

    getLinesChannel

--- main.go
+++ main.go
@@ -16,31 +16,43 @@ func main() {
 	if err != nil {
 		log.Fatalf("Couldn't read message: %s", err)
 	}
-	defer file.Close()
-
 	fmt.Printf("Reading Data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
 
-	currentLineContents := ""
-	for {
-		b := make([]byte, 8)
-		n, err := file.Read(b)
-		if err != nil {
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
-			}
-			if errors.Is(err, io.EOF) {
+	ch := getLinesChannel(file)
+
+	for line := range ch {
+		fmt.Println("read:", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		currentLineContents := ""
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLineContents != "" {
+					ch <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := range len(parts) - 1 {
+				ch <- currentLineContents + parts[i]
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
 		}
-		str := string(b[:n])
-		parts := strings.Split(str, "\n")
-		for i := range len(parts) - 1 {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
-		}
-		currentLineContents += parts[len(parts)-1]
-	}
+	}()
+	return ch
 }
