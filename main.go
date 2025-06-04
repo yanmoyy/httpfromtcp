6264f07bc14c0be0ae614f74722d4ae639a6c2e9
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-06-04 20:56:28 +0900

    read line

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const inputFilePath = "messages.txt"
@@ -19,10 +20,15 @@ func main() {
 
 	fmt.Printf("Reading Data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
+
+	currentLineContents := ""
 	for {
 		b := make([]byte, 8)
 		n, err := file.Read(b)
 		if err != nil {
+			if currentLineContents != "" {
+				fmt.Printf("read: %s\n", currentLineContents)
+			}
 			if errors.Is(err, io.EOF) {
 				break
 			}
@@ -30,6 +36,11 @@ func main() {
 			break
 		}
 		str := string(b[:n])
-		fmt.Printf("read: %s\n", str)
+		parts := strings.Split(str, "\n")
+		for i := range len(parts) - 1 {
+			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
+			currentLineContents = ""
+		}
+		currentLineContents += parts[len(parts)-1]
 	}
 }
