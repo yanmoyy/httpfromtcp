dd4007994a7e36f892bf65e1a349446ce3cba60a
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-06-04 20:28:11 +0900

    read file

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("Couldn't read message: %s", err)
+	}
+	defer file.Close()
+
+	fmt.Printf("Reading Data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+	for {
+		b := make([]byte, 8)
+		n, err := file.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		str := string(b[:n])
+		fmt.Printf("read: %s\n", str)
+	}
 }
